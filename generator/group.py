0b915b6f5cef4076ce40a81a2071c0d38ab5f06d
d.bystrikov <zelezodoroznik@yandex.ru>
2018-12-20 09:39:10 +0300

    Установка и настройка IronPython + pip + py.test

--- generator/group.py
+++ generator/group.py
@@ -2,9 +2,10 @@ from model.group import Group
 import random
 import string
 import os.path
-import jsonpickle
 import getopt
 import sys
+
+import clr
 
 
 try:
@@ -14,7 +15,7 @@ except getopt.GetoptError as err:
         sys.exit(2)
 
 n = 5
-f = "data/groups.json"
+f = "data/groups.xlsx"
 
 for o, a in opts:
     if o=="-n":
@@ -28,14 +29,10 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+testdata = [Group(name="")] + [
+    Group(name=random_string("name", 10))
     for i in range(n)
     ]
 
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
-
-with open(file, "w") as out:
-    jsonpickle.set_encoder_options("json", indent=2)
-    out.write(jsonpickle.encode(testdata))
