7a859a79729d785c2f9d49057f9dc5f1ac91abe3
d.bystrikov <zelezodoroznik@yandex.ru>
2018-12-21 12:19:06 +0300

    18-50

--- test_something.py
+++ test_something.py
@@ -7,7 +7,27 @@ sys.path.append(os.path.join(project_dir, "Castle.Core.3.3.0\\lib\\net40-client\
 clr.AddReferenceByName('TestStack.White')
 
 from TestStack.White import Application
+from TestStack.White.InputDevices import Keyboard
+from TestStack.White.WindowsAPI import KeyboardInput
+from TestStack.White.UIItems.Finders import *
+
+clr.AddReferenceByName('UIAutomationTypes, Version=4.0.0.0, Culture=neutral, PublickKeyToken=31bf3856ad')
+from System.Windows.Automation import *
+
+import time
 
 def test_something():
-    Application.Launch("notepad.exe")
-    print('ok')
+    application = Application.Launch("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe")
+    main_window = application.GetWindow("Free Address Book")
+    main_window.Get(SearchCriteria.ByAutomationId("groupButton")).Click()
+    modal = main_window.ModalWindow("Group editor")
+
+    modal.Get(SearchCriteria.ByAutomationId("uxNewAddressButton")).Click()
+    modal.Get(SearchCriteria.ByControlType(ControlType.Edit)).Enter('test group')
+    Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN)
+
+    time.sleep(10)
+
+    modal.Get(SearchCriteria.ByAutomationId("uxCloseAddressButton")).Click()
+
+    main_window.Get(SearchCriteria.ByAutomationId("uxExitAddressButton")).Click()
